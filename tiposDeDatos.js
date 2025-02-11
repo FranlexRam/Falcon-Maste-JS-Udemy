7a6586ec8256f0fd80dc17f4a649e48639e658b8
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-02-10 21:04:38 -0400

    Primeras lecciones operadores matematicos.

---
+++ tiposDeDatos.js
@@ -0,0 +1,51 @@
+const nombre = 'Carlos';
+const parrafo = "Este es un parrafo";
+
+// Arrays - Arreglos
+const arreglo = ['texto',456, true, {propiedad: 'valor'}, [1,2,3,]];
+console.log(arreglo);
+
+//objeto
+const persona = {
+    nombre: 'Carlos',
+    edad: 27,
+    carro : {
+        marca: 'Toyota',
+        color: 'Gris'
+    }
+}
+
+console.log(persona);
+
+console.log(persona.carro.marca);
+
+
+//Operadores artimeticos
+/*
+% modulo
+++ Aumento
+--Disminucion
+*/
+
+/*
+Operadores de ignacion
++=   suma un numero al valor de una variable.
+-=   Resta un numero al valor de una variable.
+*=   Multiplica un numero al valor de una variable.
+/=   Divide un numero al valor de una variable.
+%=   Obtiene el sobrante de una division y lo asigna a la variable.
+*/
+
+let numero = 10;
+numero *=5;
+
+
+console.log(`El resultado es ${numero}:`);
+
+
+const resultado = 
+7>1
+? 'El primer valor es mayor que el segundo'
+: 'El segundo valor es mayor que el primero';
+
+console.log(resultado);
