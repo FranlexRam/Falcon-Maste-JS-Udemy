77dc66bb438438c4ecbb167be11f94fc43f9de40
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-02-15 20:59:19 -0400

    Practicando Arrays.

---
+++ arreglos.js
@@ -0,0 +1,13 @@
+const arreglo = ['Texto', 456.7, false, {propiedad:'valor'}, [1,2,3,4] ];
+console.log(arreglo);
+
+const amigos = ['Alejandro', 'Manuel', 'Cesar'];
+console.log(amigos[1]);
+console.log(amigos[0]);
+console.log(amigos[2]);
+
+console.log(`El arreglo tiene ${amigos.length} elementos`);
+
+amigos.push('Carlos'); //Agrega un elemento al final de un array
+
+console.log(amigos);
