2ec6c653723398bf7ab169a131dbe0d3b0dfa609
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-02-19 21:21:33 -0400

    Practicando funciones.

---
+++ funciones.js
@@ -0,0 +1,15 @@
+//**Forma #1: */
+// function saludo() {
+//     console.log("Hola!");
+// }
+
+// saludo();
+
+//**Forma #2: Asignando una funcion a una variable:
+// const saludo = function () {
+//     console.log("Hola desde la variable");
+// }
+
+//**Forma #3: Funciones tipo flecha. */
+
+const saludo = () => console.log("Saludo desde la funcion flecha!");
