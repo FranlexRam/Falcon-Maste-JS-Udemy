2cc3eb935dd92f54daeaa8f669ff239cbeb7e203
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-03-03 22:52:20 -0400

    Metodos para arreglos PARTE II.

--- metodosParaArreglos.js
+++ metodosParaArreglos.js
@@ -17,7 +17,7 @@ console.log(colores.sort());
 console.log(colores.reverse());
 
 /*  📌 .concat()
-    Permite untar dos arreglos en uno solo.
+    Permite juntar dos arreglos en uno solo.
 */
 
 const numeros = [1,3,7,83,57,70];
@@ -51,4 +51,35 @@ console.log(`${colorEliminado}// este color se elimino con el metodo`);
 
 console.log(colores[0]);
 
-//4:02:50
+/* 📌 .unshift()
+    Agrega un elemento al inicio de un arreglo y empuja los elementos.
+*/
+
+const dias = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+
+console.log(dias);
+dias.unshift('Domingo');
+console.log(dias);
+
+
+/* 📌 .splice()
+    Permite agregar un elemento en donde nosotros le indiquemos.
+    -1er parametro: Posicion en donde se quiere comenzar a insertar los elementos.
+    -2do parametro: Si queremos eliminar elementos del arreglo desde la posicion indicada.
+    -Resto de parametros: Los elementos a insertar.
+*/
+
+const amigos = ['Carlos', 'Luis', 'Daniel'];
+amigos.splice(1,2,'Franlex','Eduardo');
+
+console.log(amigos);
+
+
+/* 📌 .slice()
+    Nos permite copiar una parte de un arreglo a otro.
+    -1er parametro: Pocision desde donde queremos copiar.
+    -2do parametro: Hasta antes de que elemento copiar.
+*/
+const frutas = ['Fresa','Manzana','Pera','Uva','Durazno','Patilla','Melon'];
+const frutasFavoritas = frutas.slice(0,4);
+console.log(frutasFavoritas);
